8c4b36303f4ab6702b8a7205706b8fbe8cdf2094
Autumn <autumn@autmn.net>
2024-08-14 06:39:18 -0600

    Refactor for readability, update about

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,36 @@
 import Head from "next/head";
-import {Navigation, Footer} from "@/modules/navigation";
-import {add_bot, open_server, open_github} from "@/modules/links.js";
+import { Navigation, Footer } from "@/modules/navigation";
+import { add_bot, open_server, open_github } from "@/modules/links.js";
 export default function Main() {
   return (
-      <>
-          <Head>
-              <title>RoWhoIs</title>
-              <meta name="description" content="The most advanced Roblox lookup Discord utility."/>
-              <meta name="viewport" content="width=device-width, initial-scale=1"/>
-              <link rel="icon" href="/favicon.png"/>
-          </Head>
-          <Navigation />
-          <div className="content">
-              <img src="/rwi-pfp.png" alt="RoWhoIs logo" className="responsive"/>
-              <h1>RoWhoIs</h1>
-              <p>The most advanced Roblox lookup Discord utility.</p>
-              <div>
-                  <button type="button" onClick={add_bot}><span></span>Add to your server</button>
-                  <button type="button" onClick={open_server}><span></span>Support Server</button>
-                  <button type="button" onClick={open_github}><span></span>Github Page</button>
-              </div>
-          </div>
-          <Footer />
-      </>
+    <>
+      <Head>
+        <title>RoWhoIs</title>
+        <meta
+          name="description"
+          content="The most advanced Roblox lookup Discord utility."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Navigation />
+      <div className="content">
+        <img src="/rwi-pfp.png" alt="RoWhoIs logo" className="responsive" />
+        <h1>RoWhoIs</h1>
+        <p>The most advanced Roblox lookup Discord utility.</p>
+        <div>
+          <button type="button" onClick={add_bot}>
+            <span></span>Add to your server
+          </button>
+          <button type="button" onClick={open_server}>
+            <span></span>Support Server
+          </button>
+          <button type="button" onClick={open_github}>
+            <span></span>Github Page
+          </button>
+        </div>
+      </div>
+      <Footer />
+    </>
   );
 }
--- pages/plus.js
+++ pages/plus.js
@@ -2,26 +2,35 @@ import Head from "next/head";
 import { Footer, Navigation } from "@/modules/navigation";
 import { upsell_premium } from "@/modules/links.js";
 export default function Main() {
-    return (
-        <>
-            <Head>
-                <title>RoWhoIs+</title>
-                <meta name="description" content="RoWhoIs+: RoWhoIs to the next level" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.png" />
-            </Head>
-            <Navigation />
-            <div className="plus column-content">
-                <div className="column-section">
-                    <img src="/rwip-ico.webp" alt="RoWhoIs logo" />
-                    <div className="text-container">
-                        <h1>RoWhoIs+</h1>
-                        <p>RoWhoIs to the next level.<br />No cooldowns, more commands, and more data.</p>
-                        <button type="button" onClick={upsell_premium}><span></span>Get RoWhoIs+</button>
-                    </div>
-                </div>
-            </div>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>RoWhoIs+</title>
+        <meta
+          name="description"
+          content="RoWhoIs+: RoWhoIs to the next level"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Navigation />
+      <div className="plus column-content">
+        <div className="column-section">
+          <img src="/rwip-ico.webp" alt="RoWhoIs logo" />
+          <div className="text-container">
+            <h1>RoWhoIs+</h1>
+            <p>
+              RoWhoIs to the next level.
+              <br />
+              No cooldowns, more commands, and more data.
+            </p>
+            <button type="button" onClick={upsell_premium}>
+              <span></span>Get RoWhoIs+
+            </button>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
